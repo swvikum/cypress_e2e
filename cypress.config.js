7a731c244a3e064ea24408689d6bbfa464a0d952
swvikum <swvikum1993@gmail.com>
2023-12-30 20:45:01 +1100

    added mocha-awesome reporting

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,17 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-    baseUrl: "http://localhost:8080/parabank"
+    baseUrl: "http://localhost:8080/parabank",
+    chromeWebSecurity: false,
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 120000,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json'
+    }
   },
   component: {
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
